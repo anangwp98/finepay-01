c0c20935028d9817eab688ebc7887203d67289f0
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-10-20 00:35:36 +0700

    register

---
+++ dashboard/admin/proses.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+$errors = array(); 
+
+include('../koneksi.php');
+
+if (isset($_POST['reg_admin'])) {
+  $id = mysqli_real_escape_string($koneksi, $_POST['id']);
+  $nama = mysqli_real_escape_string($koneksi, $_POST['nama']);
+  $username = mysqli_real_escape_string($koneksi, $_POST['username']);
+  $password = mysqli_real_escape_string($koneksi, $_POST['pass']);
+
+  $user_check_query = "SELECT * FROM admin WHERE nama='$nama' OR username='$username' LIMIT 1";
+  $result = mysqli_query($koneksi, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if (count($errors) == 0) {
+    $password = md5($password);
+    
+  	$query = "INSERT INTO admin VALUES('$id', '$nama', '$username','$password')";
+    mysqli_query($koneksi, $query);
+    header('register.html');
+  }
+}
---
+++ dashboard/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "finepay";
+    $koneksi = mysqli_connect($host,$user,$pass,$db);
+?>
