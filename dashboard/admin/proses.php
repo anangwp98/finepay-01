6654381e9a4b7bcebc489d191af745325cca4706
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-10-20 12:23:36 +0700

    sql

--- dashboard/admin/proses.php
+++ dashboard/admin/proses.php
@@ -6,7 +6,6 @@ $errors = array();
 include('../koneksi.php');
 
 if (isset($_POST['reg_admin'])) {
-  $id = mysqli_real_escape_string($koneksi, $_POST['id']);
   $nama = mysqli_real_escape_string($koneksi, $_POST['nama']);
   $username = mysqli_real_escape_string($koneksi, $_POST['username']);
   $password = mysqli_real_escape_string($koneksi, $_POST['pass']);
@@ -14,12 +13,19 @@ if (isset($_POST['reg_admin'])) {
   $user_check_query = "SELECT * FROM admin WHERE nama='$nama' OR username='$username' LIMIT 1";
   $result = mysqli_query($koneksi, $user_check_query);
   $user = mysqli_fetch_assoc($result);
-  
+
   if (count($errors) == 0) {
     $password = md5($password);
     
-  	$query = "INSERT INTO admin VALUES('$id', '$nama', '$username','$password')";
+    $low_username = strtolower($username);
+
+    $new_auto_increment=$low_username;
+    
+    $date = date('dmY');
+    $id="fn".$new_auto_increment.$date;
+
+  	$query = "INSERT INTO admin VALUES('$id','$nama', '$low_username','$password')";
     mysqli_query($koneksi, $query);
-    header('register.html');
+    header('location: ./register.html');
   }
 }
