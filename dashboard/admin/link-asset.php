394c226ed7d902a3b609ed11f31ee79f43221997
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2020-01-07 20:40:13 +0700

    favicon

--- dashboard/admin/link-asset.php
+++ dashboard/admin/link-asset.php
@@ -1,5 +1,19 @@
-<!-- Favicon -->
-<link href="../assets/img/brand/favicon.png" rel="icon" type="image/png">
+<!-- Favicon --> 
+
+<?php
+  $id_user_admin = $_SESSION['id'];
+  $sqlImg = "SELECT * FROM profil_img WHERE id_user='$id_user_admin'";
+  $resultImg = mysqli_query($koneksi, $sqlImg);
+
+  while ($rowimg = mysqli_fetch_assoc($resultImg)) {
+      if ( $rowimg['status'] > 0) {
+        $linkFavicon= "../assets/img/theme/".$rowimg['status']."";
+      } else {
+        echo "BELUM ADA FOTO";
+      }
+  }
+?>
+<link href="<?php echo $linkFavicon ?>" rel="icon" type="image/png">
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
   <!-- Icons -->
