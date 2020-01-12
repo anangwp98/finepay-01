018be4ddb151c665bd90f2eb348d97705fc532e9
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2020-01-12 18:30:44 +0700

    Update sidebar-admin.php

--- dashboard/admin/sidebar-admin.php
+++ dashboard/admin/sidebar-admin.php
@@ -111,23 +111,23 @@
         <ul class="navbar-nav mb-md-3">
           <li class="nav-item">
             <a class="nav-link" href="./view-report-user.php" target="_blank">
-              <i class="fab fa-pied-piper-hat"></i> Data User
+            <i class="fas fa-print"></i> Data User
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="./view-report-barang.php">
-              <i class="fab fa-pied-piper-hat"></i> Data Barang
+            <i class="fas fa-print"></i>Data Barang
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="./view-report-pesanan.php" target="_blank">
-              <i class="fab fa-pied-piper-hat"></i> Data Pesanan
+            <i class="fas fa-print"></i> Data Pesanan
             </a>
           </li>
           
           <li class="nav-item">
             <a class="nav-link" href="./ai-view-user.php">
-              <i class="fab fa-pied-piper-hat"></i> Data Topup
+            <i class="fas fa-print"></i>Data Topup
             </a>
           </li>
         </ul>
